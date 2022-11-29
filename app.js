468e7fff7e3df8cab21bc1670f6bdbedc02b8548
Uhsendin <chilebr99@gmail.com>
2022-11-28 20:44:47 -0500

    Moved order html to JS forEach function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { menuArray } from "./data";
+import { menuArray } from "./data.js";
 
 const menu = document.getElementById("menu")
 const order = document.getElementById("order")
@@ -6,4 +6,33 @@ const order = document.getElementById("order")
 
 function orderHtml() {
     let orderHtmlLayout = ""
+
+    menuArray.forEach(function(order) {
+
+        orderHtmlLayout += `  <div class="menu-container" id="menu-container">
+        <div class="left-div">
+          <div class="images">
+            <span>${order.emoji}</span>
+          </div>
+          <div class="menu-text">
+            <p>${order.name}</p>
+            <p class="sub-text">${order.ingredients}</p>
+            <p>${order.price}<p>
+          </div>
+        </div>
+        <div class="add-btn">
+          <button>+</button>
+        </div>
+      </div>
+    <hr>`
+    })
+
+return orderHtmlLayout
 }
+
+function render() {
+    document.getElementById("menu-content").innerHTML = orderHtml()
+
+}
+
+render()
