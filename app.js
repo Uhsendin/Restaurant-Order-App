7bc0de051da1fa9432e5e6f2af7a1c3d9c33ed04
Uhsendin <chilebr99@gmail.com>
2022-11-29 23:35:37 -0500

    Added order total html & CSS

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 import { menuArray } from "./data.js";
 
-const menu = document.getElementById("menu")
-const order = document.getElementById("order")
-
+const menu = document.getElementById("menu");
+const order = document.getElementById("order");
 
 function orderHtml() {
-    let orderHtmlLayout = ""
+  let orderHtmlLayout = "";
 
-    menuArray.forEach(function(order) {
-
-        orderHtmlLayout += `  <div class="menu-container" id="menu-container">
+  menuArray.forEach(function (order) {
+    orderHtmlLayout += `  <div class="menu-container" id="menu-container">
         <div class="left-div">
           <div class="images">
-            <span>${order.emoji}</span>
+            <span class="emoji">${order.emoji}</span>
           </div>
           <div class="menu-text">
             <p>${order.name}</p>
@@ -24,15 +22,42 @@ function orderHtml() {
           <button>+</button>
         </div>
       </div>
-    <hr>`
-    })
+    <hr>`;
+  });
 
-return orderHtmlLayout
+  return orderHtmlLayout;
+}
+
+function orderTotal() {
+  let total = "";
+
+  total += `<div class="order-title">Your Order</div>
+  <div class="order-container">
+  <div class="order-top">
+    <div class="items-ordered">
+      <p>Pizza <span>remove</span></p>
+      <p>Beer <span>remove</span></p>
+    </div>
+    <div class="price-of-order">
+      <p>$14</p>
+      <p>$12</p>
+    </div>
+    </div>
+    <hr />
+    <div class="order-total">
+      <p>Total Price:</p>
+      <p>$26</p>
+      </div>
+      <div class="complete-order-btn">
+        <button>Complete Order</button>
+      </div>
+  </div>`;
+  return total;
 }
 
 function render() {
-    document.getElementById("menu-content").innerHTML = orderHtml()
-
+  document.getElementById("menu-content").innerHTML = orderHtml();
+  document.querySelector("footer").innerHTML = orderTotal();
 }
 
-render()
+render();
