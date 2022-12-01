163f571eeb9421162b439f1dbb0923d0949b6e6c
Uhsendin <chilebr99@gmail.com>
2022-11-30 19:41:36 -0500

    Added order total functionality

--- app.js
+++ app.js
@@ -1,27 +1,66 @@
 import { menuArray } from "./data.js";
 
-const menu = document.getElementById("menu");
-const order = document.getElementById("order");
+let orderArray = []
+let totalPrice = 0
+const totalPriceEl = document.getElementById("total-price")
+
+document.addEventListener("click", function(e) {
+  if (e.target.dataset.food) {
+    handleFoodClick(e.target.dataset.food)
+  } else if (e.target.dataset.remove) {
+
+  }
+})
+
+
+function handleFoodClick(foodItem) {
+let targetFoodObj = menuArray.filter(function (food) {
+  return foodItem == food.id
+})[0]
+
+if (!orderArray.includes(targetFoodObj)) {
+orderArray.push(targetFoodObj)
+targetFoodObj.quantity = 1
+} else {
+  targetFoodObj.quantity++
+}
+
+totalPrice += targetFoodObj.price
+totalPriceEl.textContent = `${totalPrice}`
+
+if (menuArray.length > 0) {
+  document.getElementById("total").classList.remove("hidden")
+  document.getElementById("order").classList.remove("hidden")
+}
+
+document.getElementsByClassName("food-quantity").textContent = `X${targetFoodObj.quantity}`
+document.getElementById("complete-btn").classList.remove("hidden")
+document.getElementById("section").classList.remove("hidden")
+render()
+
+
+
+}
 
 function orderHtml() {
   let orderHtmlLayout = "";
 
   menuArray.forEach(function (order) {
     orderHtmlLayout += `  <div class="menu-container" id="menu-container">
-        <div class="left-div">
-          <div class="images">
-            <span class="emoji">${order.emoji}</span>
-          </div>
-          <div class="menu-text">
-            <p>${order.name}</p>
-            <p class="sub-text">${order.ingredients}</p>
-            <p>${order.price}<p>
-          </div>
-        </div>
-        <div class="add-btn">
-          <button>+</button>
-        </div>
-      </div>
+    <div class="left-div">
+    <div class="images">
+    <span class="emoji">${order.emoji}</span>
+    </div>
+    <div class="menu-text">
+    <p>${order.name}</p>
+    <p class="sub-text">${order.ingredients}</p>
+    <p>$${order.price}<p>
+    </div>
+    </div>
+    <div class="add-btn">
+    <button data-food="${order.id}" class="btns">+</button>
+    </div>
+    </div>
     <hr>`;
   });
 
@@ -29,35 +68,32 @@ function orderHtml() {
 }
 
 function orderTotal() {
-  let total = "";
+  let total = ``;
 
-  total += `<div class="order-title">Your Order</div>
-  <div class="order-container">
-  <div class="order-top">
-    <div class="items-ordered">
-      <p>Pizza <span>remove</span></p>
-      <p>Beer <span>remove</span></p>
+  menuArray.forEach(function (order) {
+    total += `<div class="order-item">
+    <div class="order-item-content1">
+      <p class="food-name">${order.name}</p>
+      <button class="remove-from-order" data-remove="${order.id}">
+        remove
+      </button>
+      <p class="food-quantity">X${order.quantity}</p>
     </div>
-    <div class="price-of-order">
-      <p>$14</p>
-      <p>$12</p>
+
+    <div class="order-item-content-2">
+      <p class="food-price">$${order.price * order.quantity}</p>
     </div>
-    </div>
-    <hr id="total-hr" />
-    <div class="order-total">
-      <p>Total Price:</p>
-      <p>$26</p>
-      </div>
-      <div class="complete-order-btn">
-        <button>Complete Order</button>
-      </div>
-  </div>`;
+  </div>
+  `;
+  });
   return total;
 }
 
+
 function render() {
-  document.getElementById("menu-content").innerHTML = orderHtml();
-  document.querySelector("footer").innerHTML = orderTotal();
+  document.getElementById('menu-content').innerHTML = orderHtml()
+  document.getElementById('order-display').innerHTML = orderTotal()
 }
 
-render();
+render()
+
